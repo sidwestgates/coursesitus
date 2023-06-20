fbce5b3c9cc7041c32c845607a14640687bc39b2
sidwestgates <137149520+sidwestgates@users.noreply.github.com>
2023-06-20 12:56:32 +0530

    adding component
    adding component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  template: `
+    <h2>About Us</h2>
+    <p>This is the about us page content.</p>
+  `
+})
+export class AboutComponent {
+
+}
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  template: `
+  <h2>Contact Us</h2>
+  <p>This is the contact us page content.</p>
+`
+})
+export class ContactComponent {
+
+}
